ebf1e051418cde2d82c865f6e256faf2e145b328
maucor919 <maurocoronel919@gmail.com>
2023-06-08 20:07:13 -0300

    empezando

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express');
+
+let path = require('path');
+
+let app = express();
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.listen(3030, () => { console.log('Servidor corriendo en el puerto 3030') });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
