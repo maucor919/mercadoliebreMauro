e8797be70d59fb71627da3aca2ebc7f7ba7544b2
maucor919 <maurocoronel919@gmail.com>
2023-06-08 23:59:00 -0300

    modificado

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ let app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3030, () => { console.log('Servidor corriendo en el puerto 3030') });
-
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'));
 });
+
+app.listen(3030, () => { console.log('Servidor corriendo en el puerto 3030') });
