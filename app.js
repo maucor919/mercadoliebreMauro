fea4831232381aed7420a0b1073f3c65b1971e0b
maucor919 <maurocoronel919@gmail.com>
2023-06-09 02:25:47 -0300

    avance

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views','home.html'));
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
-
-app.listen(process.env.PORT || 3000, () => { console.log('Servidor corriendo en el puerto 3000') });
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => { console.log('Servidor corriendo en el puerto' + PORT) });
