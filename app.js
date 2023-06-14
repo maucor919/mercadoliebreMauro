f40d892b2b568684d6283296d2c1c15b0aec6f61
maucor919 <maurocoronel919@gmail.com>
2023-06-13 23:12:24 -0300

    register

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
+
+/*app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'register.html'));
+});*/
+
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => { console.log('Servidor corriendo en el puerto' + PORT) });
